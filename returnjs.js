206ac50ce3ad6345a9f86fb91eb17ca42cf9be40
nghuyen dinh quoc uyy <90595569+tonyuyndqOM@users.noreply.github.com>
2023-02-13 18:22:57 +0700

    Update returnjs.js

--- returnjs.js
+++ returnjs.js
@@ -8,19 +8,45 @@
             return {
                 "id": "returnjs",
                 "name": "returnjs",
+                "color1": "#aa1111",
+                "color2": "#aa1111",
+                "color3": "#aa1111",
                 "blocks": [
                     {
                         "opcode":"pi",
                         "blockType": "reporter",
                         "text":"pi",
                         "arguments":{}
+                    },
+                    {
+                        "opcode":"e",
+                        "blockType": "reporter",
+                        "text":"e",
+                        "arguments":{}
+                    }
+                    {
+                        "opcode":"evaluate",
+                        "blockType": "reporter",
+                        "text":"evaluate [value]",
+                        "arguments":{
+                            "value":{
+                                "type":"string",
+                                "defaultvalue": "5+5"
+                            }
+                        }
                     }
                 ]
             };
         }
 
         pi(){
-            return(Math.PI);
+            return Math.PI;
+        }
+        e(){
+            return Math.E;
+        }
+        evaluate({value}){
+            return eval(value);
         }
     }
 
