0f852e3383774a7868d8abc6fdeb8eafcd938a67
nghuyen dinh quoc uyy <90595569+tonyuyndqOM@users.noreply.github.com>
2023-03-10 22:44:41 +0700

    Create

---
+++ customjs.js
@@ -0,0 +1,120 @@
+(function (Scratch){
+    'use strict';
+    var x = 0;
+    
+    class customjs {
+        constructor() {
+        }
+    
+        getInfo() {
+            return {
+                color1: "#FFAA00",
+                color2: "#FFAA00",
+                "id": "customjs",
+                "name": "customjs",
+                "description":"a very normal extension",
+                "blocks": [
+                    {
+                        "opcode":"pi",
+                        "blockType": "reporter",
+                        "text":"pi",
+                        "arguments":{}
+                    },
+                    {
+                        "opcode":"e",
+                        "blockType": "reporter",
+                        "text":"e",
+                        "arguments":{}
+                    },
+                    {
+                        "opcode":"getx",
+                        "blockType": "reporter",
+                        "text":"x",
+                        "arguments":{}
+                    },
+                    {
+                        "opcode":"set",
+                        "blockType": "command",
+                        "text":"set x to [VAL]",
+                        "arguments":{
+                            "VAL":{
+                                "type":"number",
+                                "defaultValue": "20"
+                            }
+                        }
+                    },
+                    {
+                        "opcode":"evaluate",
+                        "blockType": "reporter",
+                        "text":"evaluate [VAL]",
+                        "arguments":{
+                            "VAL":{
+                                "type":"string",
+                                "defaultValue": "5+5"
+                            }
+                        }
+                    },
+                    {
+                        "opcode":"mixedfr",
+                        "blockType":"reporter",
+                        "text":"mixed fraction [WHOLE],[NUM] over [DEN]",
+                        "arguments":{
+                            "WHOLE":{
+                                "type":"number",
+                                "defaultValue": "1"
+                            },
+                            "NUM":{
+                                "type":"number",
+                                "defaultValue": "3"
+                            },
+                            "DEN":{
+                                "type":"number",
+                                "defaultValue": "4"
+                            }
+                        }
+                    },
+                    {
+                        "opcode":"power",
+                        "blockType":"reporter",
+                        "text":"[F]^[S]",
+                        "arguments":{
+                            "F":{
+                                "type":"number",
+                                "defaultValue": "2"
+                            },
+                            "S":{
+                                "type":"number",
+                                "defaultValue": "3"
+                            }
+                        }
+                    }
+                ]
+            };
+        }
+
+        pi(){
+            return Math.PI;
+        }
+        e(){
+            return Math.E;
+        }
+        getx(){
+            return x;
+        }
+        set(args){
+            x = args.VAL;
+        }
+        evaluate(args){
+            return eval(args.VAL);
+        }
+        mixedfr(args){
+            return args.WHOLE + args.NUM / args.DEN;
+        }
+        power(args){
+            return args.F ** args.S;
+        }
+    }
+
+    Scratch.extensions.register(new customjs());
+})(Scratch);
+/**/
